f3da2811a65a0b5893581603a157eaa0baf60e27
Douglas Cardoso <douglas.dcm@gmail.com>
2024-10-11 03:04:25 -0300

    add arguments

--- ferramentas/02_selenium/test_selenium.py
+++ ferramentas/02_selenium/test_selenium.py
@@ -10,14 +10,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 # ~/.cache/selenium/chromedriver/linux64/129.0.6668.100
 def test_demo():
     options = ChromeOptions()
-    options.add_argument("--headless=new")
+    options.headless = True
+    # options.add_argument("--headless=new")
     # options.add_argument("--disable-dev-shm-usage")
     # options.add_argument("start-maximized")
     # options.add_argument("disable-infobars")
     # options.add_argument("--disable-extensions")
-    # options.add_argument("--disable-dev-shm-usage")
     # options.add_argument("--no-sandbox")
-    options.add_argument("--remote-debugging-pipe")
+    # options.add_argument("--remote-debugging-pipe")
 
     driver = webdriver.Chrome(
         options=options, service=ChromeService(ChromeDriverManager().install())
